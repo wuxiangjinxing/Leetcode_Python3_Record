07f1f2d822b342a0e4a944dcd072a8178b87cff5
wuxiangjinxing <caoliaoran@gmail.com>
2019-10-10 14:30:58 -0400

    Update union.py

--- union.py
+++ union.py
@@ -5,13 +5,24 @@
 
 # Disjoint union set is a data structure that tracks a set of data partitioned in a number of non-overlapping subsets. The data structure supports find (locate the subset for a given data) and union (combine two subsets) operations.
 
-class union:
+class DSU:
     def __init__(self, nodes):
         self.nodes = nodes
         self.graph = collections.defaultdict(list)
     
     def addEdge(self, u, v):
         self.graph[u].append(v)
+	
+    def find(self, subsets, i):
+        if subsets[i] == -1:
+            return i
+        else:
+            return self.find(subsets, subsets[i])
+        
+    def union(self, subsets, x, y):
+        x_set = self.find(subsets,x)
+        y_set = self.find(subsets,y)
+        subsets[x_set] = y_set
 	
 	
         
