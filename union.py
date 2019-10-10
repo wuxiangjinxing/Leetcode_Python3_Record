7b600acb1ed685448e90f52337e067b0a2145795
wuxiangjinxing <caoliaoran@gmail.com>
2019-10-10 14:23:48 -0400

    Update union.py

--- union.py
+++ union.py
@@ -1,2 +1,18 @@
 # 354. Russian Doll Envelopes
 # 952. Largest Component Size by Common Factor
+# 684. Redundant Connection
+# 685. Redundant Connection II
+
+# Disjoint union set is a data structure that tracks a set of data partitioned in a number of non-overlapping subsets. The data structure supports find (locate the subset for a given data) and union (combine two subsets) operations.
+
+class union:
+    def __init__(self, nodes):
+        self.nodes = nodes
+        self.graph = collections.defaultdict(list)
+    
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+	
+	
+        
+    
