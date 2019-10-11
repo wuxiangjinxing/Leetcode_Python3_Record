4266fdb6423c52113a0eb26e76c7dffab5cb6267
wuxiangjinxing <caoliaoran@gmail.com>
2019-10-11 11:27:22 -0400

    Create misc.py

---
+++ misc.py
@@ -0,0 +1,13 @@
+# A series of useful small functions
+
+# find all factors of a given integer in order
+def printDivisors(n):
+    res = []
+    list = []
+    st = 2 if n % 2 == 0 else 3
+    for i in range(1, int(math.sqrt(n) + 1), 2):   
+        if (n % i == 0): 
+            if (n / i != i):
+                list.append(n//i)
+            res.append(i)
+    return res + list[::-1]
