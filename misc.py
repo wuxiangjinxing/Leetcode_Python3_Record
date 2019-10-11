490c279729bebe0abc0d2c158149c8ac84a1d2a3
wuxiangjinxing <caoliaoran@gmail.com>
2019-10-11 19:47:37 -0400

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,5 +1,10 @@
 # A series of useful small functions
 
+# Combine two iterables using zip()
+a = [1, 2, 3]
+b = [4, 5, 6]
+zip(a,b) # [(1,4), (2,5), (3,6)]
+
 # find all factors of a given integer in order
 def printDivisors(n):
     res = []
