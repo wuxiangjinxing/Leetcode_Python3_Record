23d4ad8d4bd337d368619afd52cd193996bce39b
wuxiangjinxing <caoliaoran@gmail.com>
2019-12-19 15:01:52 -0500

    Create QuickSort.py

---
+++ QuickSort.py
@@ -0,0 +1,9 @@
+    def partition(arr, l, r):
+        pivot = arr[r]
+        i = l - 1
+        for j in range(l, r):
+            if arr[j] <= pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i+1], arr[r] = arr[r], arr[i+1]
+        return i + 1
